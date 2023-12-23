eae6be21ba350594ad74d4ee68054d3c6a2200d2
Vimarsh <vimarsh244@gmail.com>
2023-12-23 21:44:33 +0530

    build transformer added

--- train.py
+++ train.py
@@ -8,6 +8,9 @@ from tokenizers.models import WordLevel
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.trainers import WordLevelTrainer
 
+from dataset import BilingualDataset, casual_mask
+from model import build_transformer
+
 from pathlib import Path
 
 # requirements.txt >>
@@ -52,6 +55,35 @@ def get_ds(config):
     train_ds_size = int(0.9 * len(ds_raw))
     val_ds_size = len(ds_raw) - train_ds_size
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+    
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    # from dataset.py > BilingualDataset
+    
+    max_len_src = 0
+    max_len_tgt = 0
+    
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+        
+    print(f'Max Length of Source: {max_len_src}')
+    print(f'Max Length of Target: {max_len_tgt}')
+    
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=False) # batch size 1 cause each sentence to be processed one at a time
+    
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], config['d_model']) # rest of parameters are already predefined in the function
+    return model
+
+
+    
     
     
     
